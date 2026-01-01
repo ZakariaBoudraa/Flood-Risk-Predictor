d61edb4155717c669e34ae6056f5092c0ec5cf43
Zakaria Boudraa <zakariaboudraa@Ordinateur-portable-de-Zakaria.local>
2026-01-01 17:29:12 -0500

    first attempt for completion of rolling_features()

--- data/data_processing/process.py
+++ data/data_processing/process.py
@@ -1,6 +1,38 @@
 import pandas
 import numpy
 
+def impossible_values(data):
+    ranges = {"SM_ROOTZONE": (0, 1), "SM_SURFACE": (0, 1), "PRECTOTCORR": (0, 500), "T2M_MAX": (-50, 60),
+               "T2M_MIN": (-50, 60), "QV2M": (0, 40), "RH2M": (0, 100), "WS10M": (0, 70), "PS": (80, 120)}
+    for feature, (min, max) in ranges.items():
+        for index, value in data[feature].items():
+            if value < min or value > max:
+                print(f"Impossible value in {feature} at line {index}: {value}")
+
+def rolling_features(data):
+    data["PRECTOTCORR_1d"] = data.groupby("CITY")["PRECTOTCORR"].shift(1)
+    data["PRECTOTCORR_3d"] = data.groupby("CITY")["PRECTOTCORR"].shift(1).rolling(3).sum()
+    data["PRECTOTCORR_7d"] = data.groupby("CITY")["PRECTOTCORR"].shift(1).rolling(7).sum()
+    data["PRECTOTCORR_14d"] = data.groupby("CITY")["PRECTOTCORR"].shift(1).rolling(14).sum()
+
+    data["SM_ROOTZONE_3d_MEAN"] = data.groupby("CITY")["SM_ROOTZONE"].shift(1).rolling(3).mean()
+    data["SM_SURFACE_3d_MEAN"] = data.groupby("CITY")["SM_SURFACE"].shift(1).rolling(3).mean()
+
+    data["SM_ROOTZONE_7d_MEAN"] = data.groupby("CITY")["SM_ROOTZONE"].shift(1).rolling(7).mean()
+    data["SM_SURFACE_7d_MEAN"] = data.groupby("CITY")["SM_SURFACE"].shift(1).rolling(7).mean()
+
+    data["PRECTOTCORR_1d_CHANGE"] = data.grouby("CITY")["PRECTOTCORR"].diff()
+    data["SM_ROOTZONE_1d_CHANGE"] = data.grouby("CITY")["SM_ROOTZONE"].diff()
+    data["SM_SURFACE_1d_CHANGE"] = data.grouby("CITY")["SM_SURFACE"].diff()
+
+    data["DAY_OF_YEAR"] = data["DATE"].dt.dayofyear
+    data["DOFY_SINE"] = numpy.sin((2 * numpy.pi + data["DAY_OF_YEAR"]) / 365.25)
+    data["DOFY_COSINE"] = numpy.cos((2 * numpy.pi + data["DAY_OF_YEAR"]) / 365.25)
+
+    data["FLOOD_IN_LAST_30d"] = data.groupby("CITY")["FLOOD"].shift(1).rolling(30).max().fillna(0).astype(int)
+
+    data = data.dropna().reset_index(drop=True)
+
 def compute(data):
     count = data["FLOOD"].value_counts(normalize=False)
     mean = data.groupby("CITY")["FLOOD"].mean()
